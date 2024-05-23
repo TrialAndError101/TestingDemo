795a621967281aa4293ddd11c897c9f3b728c480
TrialAndError101 <130543640+TrialAndError101@users.noreply.github.com>
2024-05-23 16:56:04 +0800

    first commit

---
+++ TestDemoConsole/TestDemoConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestDemoConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
